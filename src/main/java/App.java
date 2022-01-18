7f0ecf222187c479a8b03f988d26ea679d400553
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =(HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        boolean comprassion = bean == bean2;
+        Cat bean11 = (Cat) applicationContext.getBean("cat");
+        System.out.println(comprassion);
+
+        Cat bean22 = (Cat) applicationContext.getBean("cat");
+        boolean comprassion2 = bean11 == bean22;
+        System.out.println(bean22.getMessage());
+        System.out.println(bean11.getMessage());
+        System.out.println(comprassion2);
+
     }
 }
